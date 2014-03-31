6256c8b55ced27ee0049aae5c4c508618d254b6c
Marian Marinov <mm@yuhu.biz>
2014-03-31 19:26:34 +0300

    kernel/printk/printk.c: Remove the already depricated CAP_SYS_ADMIN

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -388,17 +388,6 @@ static int check_syslog_permissions(int type, bool from_file)
 	if (syslog_action_restricted(type)) {
 		if (capable(CAP_SYSLOG))
 			return 0;
-		/*
-		 * For historical reasons, accept CAP_SYS_ADMIN too, with
-		 * a warning.
-		 */
-		if (capable(CAP_SYS_ADMIN)) {
-			pr_warn_once("%s (%d): Attempt to access syslog with "
-				     "CAP_SYS_ADMIN but no CAP_SYSLOG "
-				     "(deprecated).\n",
-				 current->comm, task_pid_nr(current));
-			return 0;
-		}
 		return -EPERM;
 	}
 	return security_syslog(type);
