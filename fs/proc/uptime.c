89fc10098e44215fa730ac068dc3d815ca539cc2
Marian Marinov <mm@yuhu.biz>
2014-04-14 15:36:00 +0300

    fs/proc/uptime.c: Show the uptime of the container If the task is in a memory cgroup and the group is not / we should display the uptime based on the start of the task. TODO; replace the task with the first task from the cgroup

--- fs/proc/uptime.c
+++ fs/proc/uptime.c
@@ -15,20 +15,39 @@ static int uptime_proc_show(struct seq_file *m, void *v)
 	u64 nsec;
 	u32 rem;
 	int i;
+#ifdef CONFIG_MMCG
+	struct task_struct *tsk;
+	struct cgroup_subsys_state *css = NULL;
+	tsk = current_thread_info()->task;
+	int in_cgroup = 0;
+#endif
 
 	idletime = 0;
-	for_each_possible_cpu(i)
-		idletime += (__force u64) kcpustat_cpu(i).cpustat[CPUTIME_IDLE];
+#ifdef CONFIG_MEMCG
+	if (tsk != NULL) {
+		css = task_css(tsk, mem_cgroup_subsys_id);
+		if (strlen(css->cgroup->name->name) > 1) {
+			uptime = tsk->real_start_time;
+//			uptime = tsk->start_time;
+		}
+	} else {
+#endif
+		for_each_possible_cpu(i)
+			idletime += (__force u64) kcpustat_cpu(i).cpustat[CPUTIME_IDLE];
 
-	get_monotonic_boottime(&uptime);
-	nsec = cputime64_to_jiffies64(idletime) * TICK_NSEC;
-	idle.tv_sec = div_u64_rem(nsec, NSEC_PER_SEC, &rem);
-	idle.tv_nsec = rem;
+		get_monotonic_boottime(&uptime);
+		nsec = cputime64_to_jiffies64(idletime) * TICK_NSEC;
+		idle.tv_sec = div_u64_rem(nsec, NSEC_PER_SEC, &rem);
+		idle.tv_nsec = rem;
+#ifdef CONFIG_MEMCG
+	}
+#endif
+
 	seq_printf(m, "%lu.%02lu %lu.%02lu\n",
 			(unsigned long) uptime.tv_sec,
 			(uptime.tv_nsec / (NSEC_PER_SEC / 100)),
-			(unsigned long) idle.tv_sec,
-			(idle.tv_nsec / (NSEC_PER_SEC / 100)));
+			in_cgroup ? 0 : (unsigned long) idle.tv_sec,
+			in_cgroup ? 0 : (idle.tv_nsec / (NSEC_PER_SEC / 100)));
 	return 0;
 }
 
